4c5411a9d0d8951fe727f20331bc80c39f72a2ec
Oliver Moody <omoody@vassar.edu>
2021-05-01 15:06:37 -0400

    Initial Commit

---
+++ Shared/vassarhacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  vassarhacksApp.swift
+//  Shared
+//
+//  Created by Oliver Moody on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct vassarhacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
